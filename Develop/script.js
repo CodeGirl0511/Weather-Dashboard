f437af80bc642615f1084a0e8e7bf257c0c6a128
Swati Patel Torres <swati@Swatis-MBP.fios-router.home>
2020-07-03 18:00:08 -0400

    html/js

---
+++ Develop/script.js
@@ -0,0 +1,99 @@
+
+$(document).ready(function () {
+    function getWeather(city) {
+  
+      $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + city + "",
+        type:"GET",
+        datatype: "json",
+        success: function (data) {
+          console.log(data);
+        }
+  
+      }).then(function (response) {
+        $("#citydate").empty();
+        var city = $("<div class'city'>");
+  
+        var name = response.name;
+        var Four = $("<p>").text(name);
+        city.append(Four);
+  
+        var temp = response.main.temp; 
+        var One = $("<p>").text("Temperature: " + temp);
+        city.append(One);
+  
+        var humid = response.main.humidity;
+        var Two = $("<p>").text("Humidity: " + humid);
+        city.append(Two);
+  
+        var wind = response.wind.speed;
+        var Three = $("<p>").text("Wind Speed: " + wind);
+        city.append(three);
+  
+        $("#cityDate").prepend(city); 
+      });
+  
+      $.ajax({
+        url: 'https://api.openweathermap.org/data/2.5/forecast?q=' + ity + "api_key=19e5fc4a4d7613a575bc5901cff288a9",
+        type: "GET",
+        datatype: "json",
+        success: function (data) {
+          console.log(data);
+        }
+  
+      })
+        .then(function (response) {
+          $('#forecast').empty();
+          for (var i = 0; i < 39; i += 8) {
+            var outcome = response.list[i];
+  
+            console.log("Day " + (i + 0));
+            console.log(outcome);
+            console.log("Temp: " + outcome.main.temp);
+            console.log("Humidity: " + outcome.main.humidity);
+            console.log("Date" + outcome.dt_txt);
+  
+  
+            var date = "Date:" + outcome.dt_txt;
+            var temp = "Temperature: " + outcome.main.temp;
+            var humidity = "Humidity: " + outcome.main.humidity;
+  
+            var Card = forcast(date, temp, humidity);
+            $('#forecast').append(Card)
+          }
+        });
+    }
+    $('#button').click(function () {
+  
+      var city = $("#cityName").val();
+  
+      console.log(city);
+      getWeather(city);
+  
+      var Item = $("<li>").text(city);
+      $("#cityList").prepend(Item);
+      Item.on("click", function () {
+        getWeather(city);
+  
+      });
+  
+    });
+  
+    var forcast = (Date, Temp, Humidity) => {
+      return `<div class="col s5 m2">
+          <div class="card indigo lighten-3">
+           
+            <div class="card-content white-text">
+              <p>
+              <div>${Date}</div>
+              <br>
+              <div>${Temp}</div>
+              <br>
+              <div>${Humidity}</div>
+              </p>
+            </div>
+          </div>
+        </div>`
+    }
+  
+  });
--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Weather-Dashboard
 HW6
+
+
+Weather app.
+
+This app tell you the weather in your area.
+
+Technology used:
+
+-HTML
+-OPEN WEATHER API
+-JavaScript
+-JQuery
+-Bootstrap
+-Font Awesome
+
+
+Questions:
+
+If you have any questions feel free to reach out to: 
+
+email:Patel.swati51194@gmail.com
+
+Git Hub link :
